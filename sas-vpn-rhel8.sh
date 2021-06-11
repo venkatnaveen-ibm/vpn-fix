4e2be6090b201b4343bd55ee620bae5e89c04f7e
venkatnaveen-ibm <85454049+venkatnaveen-ibm@users.noreply.github.com>
2021-06-11 08:52:40 +0530

    Update sas-vpn-rhel8.sh

--- sas-vpn-rhel8.sh
+++ sas-vpn-rhel8.sh
@@ -4,22 +4,29 @@
 # this is individual script use at own Knowledge
 
 
-#kill current running process 
-pkill -f vpn
-pkill -f vpnui
-pkill -f agent.py
-pkill -f intranetid.py
+ping -c 1 www.google.com > /dev/null
 
-#stop Besclinet service
-sudo systemctl stop besclient
+if [ $? -eq 0 ]; then
+	echo "Internet Connected"
+else
+	echo "Check Internet connection"
+	exit 0 ;
+fi
+
+sudo pkill vpnui                # kill cisco anyconnet GUI application 
+sudo pkill -f agent.py          # kill current running agent.py script
+sudo spkill -f intranetid.py    # kill if W3 ID windows is open 
+
+sudo systemctl stop besclient   #stop Besclinet service
 
 #download required packages to current folder
-wget https://github.com/venkatnaveen-ibm/vpn-fix/raw/main/BESAgent-9.5.15.71-1.el8.1.x86_64.rpm
-wget https://github.com/venkatnaveen-ibm/vpn-fix/raw/main/ibm-config-TEM-8.4.0-1.el8.noarch.rpm
-wget https://github.com/venkatnaveen-ibm/vpn-fix/raw/main/li-el8-fix-reg.run
+wget https://github.com/venkatnaveen-ibm/vpn-fix/raw/main/BESAgent-9.5.15.71-1.el8.1.x86_64.rpm     # BESClient
+wget https://github.com/venkatnaveen-ibm/vpn-fix/raw/main/ibm-config-TEM-8.4.0-1.el8.noarch.rpm     # ibm-config-TEM
+wget https://github.com/venkatnaveen-ibm/vpn-fix/raw/main/ibm-vpn-agent-8.4-2.el8.1.noarch.rpm      # ibm-vpn-agent
+wget https://github.com/venkatnaveen-ibm/vpn-fix/raw/main/li-el8-fix-reg.run                        # registration fix 
 
 #installing packages replacing forcefully
-sudo rpm -ivh --replacefiles --replacepkgs BESAgent-9.5.15.71-1.el8.1.x86_64.rpm ibm-config-TEM-8.4.0-1.el8.noarch.rpm
+sudo rpm -ivh --replacefiles --replacepkgs BESAgent-9.5.15.71-1.el8.1.x86_64.rpm ibm-config-TEM-8.4.0-1.el8.noarch.rpm ibm-vpn-agent-8.4-2.el8.1.noarch.rpm
 
 #run reg fix 
 chmod 755 li-el8-fix-reg.run
@@ -32,14 +39,12 @@ sudo ./li-el8-fix-reg.run
 python3 /opt/ibm/registration/registration.py
 
 #connecting anyconnect vpn to temporary gateway 
-/opt/cisco/anyconnect/bin/vpn connect sasvpn06.emea.ibm.com/gettingstarted
+echo -e"connect to sasvpn06.emea.ibm/gettingstarted in cisco"
+/opt/cisco/anyconnect/bin/vpnui &
 
 #restart besclient
 sudo systemctl stop besclient
 sudo systemctl start besclient
-
-#start and run agent.py in backgroud
-python3 /opt/ibm/ibm-vpn-agent/agent.py
 
 #check log file 
 sudo cat /var/opt/BESClient/__BESData/__Global/Logs/$(date +%Y%m%d).log
